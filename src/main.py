d77b0ee8bba7cf3e0ae01e166aa08c7b4f0dba05
osip123 <131646261+osip123@users.noreply.github.com>
2024-04-24 20:23:50 +0300

    0.457 build

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 # import loadconfig from src.config.loadconfig
-
+from parser import parer
 from src.config.loadconfig import loadconfig
 
 def main() -> int:
     loadconfig()
+    parer()
     print("hellow world")
     return 0
 
